20d13ce7ef65b266897630dccf3632afe466f25c
Ved Patel <24bcs188@iiitdmj.ac.in>
2026-05-08 12:21:47 +0530

    test4

--- crates/floresta/examples/chainstate-builder.rs
+++ crates/floresta/examples/chainstate-builder.rs
@@ -23,6 +23,7 @@ const DATA_DIR: &str = "./tmp-db";
 async fn main() {
     //test3
     let network = Network::Bitcoin;
+    //test4
     let params = ChainParams::from(Network::Bitcoin);
     let genesis = genesis_block(&params);
     // Create a new chain state, which will store the accumulator and the headers chain.
