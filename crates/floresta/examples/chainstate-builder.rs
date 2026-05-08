702a3cf73da877cab3581861191bd275dd0cda0d
Ved Patel <24bcs188@iiitdmj.ac.in>
2026-05-08 11:43:34 +0530

    test-4

--- crates/floresta/examples/chainstate-builder.rs
+++ crates/floresta/examples/chainstate-builder.rs
@@ -24,6 +24,7 @@ async fn main() {
     let network = Network::Bitcoin;
     //testing 3
     let params = ChainParams::from(Network::Bitcoin);
+    //testing 4
     let genesis = genesis_block(&params);
     // Create a new chain state, which will store the accumulator and the headers chain.
     // It will be stored in the DATA_DIR directory. With this chain state, we don't keep
