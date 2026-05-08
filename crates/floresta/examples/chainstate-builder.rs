bcfdf2689fe02c8b5e9cc7e2d9c8263b5bc91ba3
Ved Patel <24bcs188@iiitdmj.ac.in>
2026-05-08 18:17:07 +0530

    demo 1

--- crates/floresta/examples/chainstate-builder.rs
+++ crates/floresta/examples/chainstate-builder.rs
@@ -18,7 +18,7 @@ use rustreexo::stump::Stump;
 const DATA_DIR: &str = "./tmp-db";
 
 #[tokio::main]
-//main function
+//deny service
 async fn main() {
     let network = Network::Bitcoin;
     let params = ChainParams::from(Network::Bitcoin);
